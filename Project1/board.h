f2e7ff57e796a8600b39665f36317bb0723e9813
yonatanzamir <65045014+yonatanzamir@users.noreply.github.com>
2020-05-17 20:37:53 +0300

    Update board.h
    ii

--- Project1/board.h
+++ Project1/board.h
@@ -1,9 +1,13 @@
-#ifndef B
-#define B
+//#ifndef B
+//#define B
 
 typedef char boardPos[2];
 
 typedef struct _boardPosArray {
 	unsigned int size;
 	boardPos *positions;
-} boardPosArray;#endif 
+} boardPosArray;
+
+
+
+//#endif 
